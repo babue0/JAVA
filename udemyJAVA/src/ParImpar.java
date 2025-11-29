e12d3ad9f35a26de138ee76676e963513208d57e
babue0 <iandpcarvalho@gmail.com>
2025-11-29 16:22:10 -0300

    praticando

---
+++ udemyJAVA/src/ParImpar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ParImpar {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("par ou impar");
+    System.out.println("digite um numero inteiro: ");
+    int num = sc.nextInt();
+
+    if (num % 2 == 0) {
+      System.out.println("par");
+    } else {
+      System.out.println("impar");
+    }
+  }
+}
---
+++ udemyJAVA/src/cobrancaTelefonica.java
@@ -0,0 +1,4 @@
+public class cobrancaTelefonica {
+  public static void main(String[] args) {
+  }
+}
