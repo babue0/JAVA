07b1cb783e383dbfca799de2ba467ce5f467d3f8
babue0 <iandpcarvalho@gmail.com>
2025-11-30 01:39:21 -0300

    praticando um pouco mais

--- udemyJAVA/src/cobrancaTelefonica.java
+++ udemyJAVA/src/cobrancaTelefonica.java
@@ -1,4 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class cobrancaTelefonica {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Locale.setDefault(Locale.US);
+
+    int min = sc.nextInt();
+
+    double conta = 50.0;
+
+    if (min > 100){
+      conta = conta + (min - 100) * 2.0;
+    }
+
+    System.out.printf("Conta = R$ %.2f", conta);
+
+
   }
 }
---
+++ udemyJAVA/src/multiplo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class multiplo {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+
+    if (A % B == 0 || B % A == 0) {
+      System.out.println("Multiplo");
+    }
+    else{
+      System.out.println("nao Multiplo");
+    }
+  }
+}
