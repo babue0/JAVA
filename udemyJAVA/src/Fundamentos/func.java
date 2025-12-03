bd0f46ec21c961cabed0e787ecbe5ce69030f834
babue0 <iandpcarvalho@gmail.com>
2025-12-02 23:52:16 -0300

    func

---
+++ udemyJAVA/src/Fundamentos/func.java
@@ -0,0 +1,42 @@
+package Fundamentos;
+
+import java.util.Scanner;
+
+public class func {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("digite 3 num: ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+
+    int higher = max(a,b,c);
+
+    showResult(higher);
+
+    sc.close();
+  }
+
+  public static int max(int x, int y, int z) {
+    int aux;
+
+    if (x > y && x > z) {
+      aux = x;
+    }
+    else if (y > z) {
+      aux = y;
+    }
+    else {
+      aux = z;
+    }
+
+    return aux;
+  }
+
+  public static void showResult(int higher) {
+    System.out.println("maior: " + higher);
+  }
+
+}
