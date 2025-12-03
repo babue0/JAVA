041e3cf304320b3e6bf025b863fb979aab1269ac
babue0 <iandpcarvalho@gmail.com>
2025-12-03 16:38:24 -0300

    pooEX1

---
+++ udemyJAVA/src/POO/EX1/entities/Triangle.java
@@ -0,0 +1,9 @@
+package POO.EX1.entities;
+
+public class Triangle {
+
+  public double a;
+  public double b;
+  public double c;
+
+}
---
+++ udemyJAVA/src/POO/application/Main.java
@@ -0,0 +1,43 @@
+package POO.application;
+
+import POO.EX1.entities.Triangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    Triangle x, y;
+    x = new Triangle();
+    y = new Triangle();
+
+    x.a = sc.nextDouble();
+    x.b = sc.nextDouble();
+    x.c = sc.nextDouble();
+
+    y.a = sc.nextDouble();
+    y.b = sc.nextDouble();
+    y.c = sc.nextDouble();
+
+    double p = (x.a + x.b + x.c) / 2.0;
+    double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+
+    p = (y.a + y.b + y.c) / 2.0;
+    double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+
+    if (areaX > areaY) {
+      System.out.println("X");
+    }else{
+      System.out.println("Y");
+    }
+    sc.close();
+  }
+}
+
+
+
